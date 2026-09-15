7d5832b07d462dc4e5b40fa79dd8d54cefa5dba2
851208518 <heatheraya3@gmail.com>
2026-09-15 22:02:59 +0000

    Submit: cpp/02-reverse-two

--- assignments/cpp/02-reverse-two/main.cpp
+++ assignments/cpp/02-reverse-two/main.cpp
@@ -2,7 +2,8 @@
 using namespace std;
 
 int main() {
-   int value;
-   cout << value << '\n';
-    cin >> value;
+    int value1, v2;
+    cin >> value1 >> v2;
+    cout << v2 << ' ' << value1 << '\n';
     return 0;
+}
